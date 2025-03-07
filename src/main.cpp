b6eddd2d7aee866c2aed9ae6fdae87c61be8cc04
ronitbatra <rbatra210@gmail.com>
2025-03-06 22:11:24 -0500

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,13 @@
 #include <chrono>
 
 int main() {
-    int port = 8080;  // Define the UDP port for order intake
+    int port = 8080;
     MatchingEngine engine(port);
 
-    // Start the matching engine (spawns the network listener)
     engine.start_engine();
 
     std::cout << "Matching Engine running on port " << port << "...\n";
 
-    // Keep the engine running to process incoming network orders
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(5));
         engine.print_order_book("AAPL");
